b63e70a923be42cfc3f51de2b4e5c1f18ac4f902
gitjulia <julee@thoughtworks.com>
2013-12-11 15:15:23 -0600

    re-added Report in config file, added StoryReportBuilder in tests

--- src/test/java/org/jbehave/business/TraderIsAlertedStories.java
+++ src/test/java/org/jbehave/business/TraderIsAlertedStories.java
@@ -1,6 +1,7 @@
 package org.jbehave.business;
 
 import static org.jbehave.core.io.CodeLocations.codeLocationFromPath;
+import static org.jbehave.core.reporters.StoryReporterBuilder.Format.*;
 
 import java.util.List;
 
@@ -11,12 +12,13 @@ import org.jbehave.core.annotations.spring.UsingSpring;
 import org.jbehave.core.embedder.Embedder;
 import org.jbehave.core.io.StoryFinder;
 import org.jbehave.core.junit.spring.SpringAnnotatedEmbedderRunner;
+import org.jbehave.core.reporters.StoryReporterBuilder;
 import org.jbehave.core.steps.ParameterConverters;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(SpringAnnotatedEmbedderRunner.class)
-@Configure(parameterConverters=ParameterConverters.EnumConverter.class)
+@Configure(parameterConverters=ParameterConverters.EnumConverter.class,storyReporterBuilder = TraderIsAlertedStories.MyReportBuilder.class)
 @UsingEmbedder(embedder = Embedder.class, generateViewAfterStories = true, ignoreFailureInStories = false, ignoreFailureInView = false)
 @UsingSpring(resources = { "org/jbehave/business/configuration.xml",
            "org/jbehave/business/tradingService-acceptancetest.xml" })
@@ -30,5 +32,11 @@ public class TraderIsAlertedStories extends InjectableEmbedder {
 	protected List<String> storyPaths() {
 		return new StoryFinder().findPaths(codeLocationFromPath("src/test/resources"), "org/jbehave/business/*.story", "");
 	}
+
+    public static class MyReportBuilder extends StoryReporterBuilder {
+        public MyReportBuilder() {
+            this.withFormats(CONSOLE, TXT, HTML, XML).withDefaultFormats();
+        }
+    }
 
 }
--- src/test/java/org/jbehave/web/TraderIsAlertedWebStories.java
+++ src/test/java/org/jbehave/web/TraderIsAlertedWebStories.java
@@ -1,6 +1,7 @@
 package org.jbehave.web;
 
 import static org.jbehave.core.io.CodeLocations.codeLocationFromPath;
+import static org.jbehave.core.reporters.StoryReporterBuilder.Format.*;
 
 import java.util.List;
 
@@ -11,12 +12,15 @@ import org.jbehave.core.annotations.spring.UsingSpring;
 import org.jbehave.core.embedder.Embedder;
 import org.jbehave.core.io.StoryFinder;
 import org.jbehave.core.junit.spring.SpringAnnotatedEmbedderRunner;
+import org.jbehave.core.reporters.StoryReporterBuilder;
 import org.jbehave.core.steps.ParameterConverters;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import javax.swing.text.html.HTML;
+
 @RunWith(SpringAnnotatedEmbedderRunner.class)
-@Configure(parameterConverters=ParameterConverters.EnumConverter.class)
+@Configure(parameterConverters=ParameterConverters.EnumConverter.class, storyReporterBuilder = TraderIsAlertedWebStories.MyReportBuilder.class)
 @UsingEmbedder(embedder = Embedder.class, generateViewAfterStories = true, ignoreFailureInStories = false, ignoreFailureInView = false)
 @UsingSpring(resources = { "org/jbehave/business/configuration.xml",
            "org/jbehave/web/tradingService-webacceptancetest.xml" })
@@ -30,5 +34,12 @@ public class TraderIsAlertedWebStories extends InjectableEmbedder {
 	protected List<String> storyPaths() {
 		return new StoryFinder().findPaths(codeLocationFromPath("src/test/resources"), "org/jbehave/web/*.story", "");
 	}
+
+    public static class MyReportBuilder extends StoryReporterBuilder {
+        public MyReportBuilder() {
+            this.withFormats(CONSOLE, TXT, HTML, XML).withDefaultFormats();
+        }
+    }
+
 
 }
