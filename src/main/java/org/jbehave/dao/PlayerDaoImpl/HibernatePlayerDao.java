9880e7150626d40dea2727f85e5dadc3239e7e86
dannielle <dannielle@gatech.edu>
2014-03-06 16:34:33 -0600

    #2 | Beccie | DDR | uncommented session factory, MAVEN CLEAN

--- src/main/java/org/jbehave/dao/PlayerDaoImpl/HibernatePlayerDao.java
+++ src/main/java/org/jbehave/dao/PlayerDaoImpl/HibernatePlayerDao.java
@@ -1,14 +1,16 @@
 package org.jbehave.dao.PlayerDaoImpl;
 
+import org.hibernate.SessionFactory;
 import org.jbehave.dao.PlayerDao;
 import org.jbehave.model.Player;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class HibernatePlayerDao implements PlayerDao {
 
-//    @Autowired(required=true)
-//    private SessionFactory sessionFactory;
+    @Autowired(required=true)
+    private SessionFactory sessionFactory;
 
     public Player findPlayer() {
 //        sessionFactory.getCurrentSession().createQuery(
@@ -16,7 +18,7 @@ public class HibernatePlayerDao implements PlayerDao {
         return null;
     }
 
-//    public void setSessionFactory(SessionFactory sessionFactory) {
+    public void setSessionFactory(SessionFactory sessionFactory) {
 //        this.sessionFactory = sessionFactory;
-//    }
+    }
 }
--- src/main/java/org/jbehave/services/impl/PlayerServiceImpl.java
+++ src/main/java/org/jbehave/services/impl/PlayerServiceImpl.java
@@ -1,7 +1,10 @@
 package org.jbehave.services.impl;
 
+import org.jbehave.dao.PlayerDao;
+import org.jbehave.dao.PlayerDaoImpl.HibernatePlayerDao;
 import org.jbehave.model.Player;
 import org.jbehave.services.PlayerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -9,14 +12,14 @@ import javax.transaction.Transactional;
 @Service
 public class PlayerServiceImpl  implements PlayerService{
 
-//    @Autowired(required=true)
-//    private PlayerDao playerDao;
+    @Autowired(required=true)
+    private PlayerDao playerDao;
 
     @Transactional
     public Player getPlayer() {
-//        playerDao = new HibernatePlayerDao();
-//        return playerDao.findPlayer();
-        return null;
+        playerDao = new HibernatePlayerDao();
+        return playerDao.findPlayer();
+//        return null;
     }
 
 
