6ba5ef4207bcc0f70db20b1e6c33eeef95e2c2e5
dannielle <dannielle@gatech.edu>
2014-03-06 16:25:54 -0600

    #2 | Beccie | DDR | removing session factory

--- src/main/java/org/jbehave/dao/PlayerDaoImpl/HibernatePlayerDao.java
+++ src/main/java/org/jbehave/dao/PlayerDaoImpl/HibernatePlayerDao.java
@@ -1,20 +1,22 @@
 package org.jbehave.dao.PlayerDaoImpl;
 
-import org.hibernate.SessionFactory;
 import org.jbehave.dao.PlayerDao;
 import org.jbehave.model.Player;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class HibernatePlayerDao implements PlayerDao {
 
-    @Autowired(required=true)
-    private SessionFactory sessionFactory;
+//    @Autowired(required=true)
+//    private SessionFactory sessionFactory;
 
     public Player findPlayer() {
-        sessionFactory.getCurrentSession().createQuery(
-                "from Player");
+//        sessionFactory.getCurrentSession().createQuery(
+//                "from Player");
         return null;
     }
+
+//    public void setSessionFactory(SessionFactory sessionFactory) {
+//        this.sessionFactory = sessionFactory;
+//    }
 }
