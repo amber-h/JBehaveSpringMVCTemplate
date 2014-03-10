a98c0519f35fe495688d0a3c95dc83533c9d9179
ahoule <thoughtworker@AH10277.local>
2014-03-10 11:52:21 -0500

    #7: Dannielle & Amber: Added Service annotation to coach service

--- src/main/java/org/jbehave/services/CoachService.java
+++ src/main/java/org/jbehave/services/CoachService.java
@@ -1,9 +1,11 @@
 package org.jbehave.services;
 
 import org.jbehave.model.Coach;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 
+@Service
 public class CoachService {
     public ArrayList<Coach> retrieveCoaches() {
         return null;
